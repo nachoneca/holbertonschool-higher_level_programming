8d659aca48891f0756062eef1a5694cdf65f3b9c
nachoneca <8666@holbertonstudents.com>
2024-05-06 22:22:07 +0000

    Creation of 8-concat_edges.py

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
- language that combines remarkable power with very clear syntax"
-print(str.replace("Python is an interpreted, interactive, object-oriented programming\
- language that combines remarkable power with very clear syntax", "object-oriented programming with Python"))
+    language that combines remarkable power with very clear syntax"
+str = str[39:55] + str[55:66] + str[-23:-17] + str[0:6]
+print(str)
