9047c3c757510e490a97994367b3887fd4351728
nachoneca <8666@holbertonstudents.com>
2024-05-08 16:28:36 +0000

    Creation of 12-fizzbuzz.py

---
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    num = [x for x in range(1, 101)]
+    for i in num:
+        if i % 5 == 0 and i % 3 == 0:
+            print("FizzBuzz", end=" ")
+        elif i % 5 == 0:
+            print("Buzz", end=" ")
+        elif i % 3 == 0:
+            print("Fizz", end=" ")
+        else:
+            print(i, end=" ")
