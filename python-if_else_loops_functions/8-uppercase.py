2ec6fc6e98bd58e5cb115be071fa6b26a63d8bcd
nachoneca <8666@holbertonstudents.com>
2024-05-08 16:19:07 +0000

    Creation of 8-uppercase.py

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
     for i in str:
-        char = ord(i)#gets ascci
+        char = ord(i)
         if char >= 97 and char <= 122:
-            char -= 32#make lttrs MAYUS
-        print("{:c}".format(char), end="")#prints every letter in MAYUS
-    print()#prints NL
+            char -= 32
+        print("{:c}".format(char), end="")
+    print()
