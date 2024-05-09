2eff7c8ad631791e4f76af2e30b44d469346d561
nachoneca <8666@holbertonstudents.com>
2024-05-09 17:50:32 +0000

    Creation of 2-args.py

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import sys
+if __name__ = "__main__":
+    numarg = len(argv)
+    if numarg == 0:
+        print("{} arguments.".format(numarg))
+    elif numarg == 1:
+        print("{} argument:\n{}: {}".format(numarg, numarg, sys.argv[1:]))
+    elif numarg > 1:
+        print("{} arguments:\n")
+        for i in range(numarg)
+            print("{}: {}".format(i, sys.argv[i]))
+
+
+
