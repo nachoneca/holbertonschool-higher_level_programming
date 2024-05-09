17f131768b339772d3719e79acea91f4020883d8
nachoneca <8666@holbertonstudents.com>
2024-05-09 16:50:17 +0000

    Fix of 1-calculation.py

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     a = 10
     b = 5
     from calculator_1 import add, sub, mul, div
-    print("{} + {}= {}".format(a, b, add(a, b)))
-    print("{} - {}= {}".formar(a, b, sub(a, b)))
-    print("{} x {}= {}".format(a, b, mul(a, b)))
-    print("{} / {}= {}".format(a, b, div(a, b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".formar(a, b, sub(a, b)))
+    print("{} x {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
