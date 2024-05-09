bbd9bf07bb1273abfc59fe7984be5659c88591c9
nachoneca <8666@holbertonstudents.com>
2024-05-09 18:01:35 +0000

    fix of 2-args.py

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -6,10 +6,10 @@ if __name__ == "__main__":
     if numarg == 0:
         print("{} arguments.".format(numarg))
     elif numarg == 1:
-        print("{} argument:\n{}: {}".format(numarg, numarg, sys.argv[1:]))
+        print("{} argument:\n{}: {}".format(numarg, numarg, sys.argv[1]))
     elif numarg > 1:
-        print("{} arguments:\n")
-        for i in range(numarg)
+        print("{} arguments:\n".format(numarg))
+        for i in range(1, numarg):
             print("{}: {}".format(i, sys.argv[i]))
 
 
