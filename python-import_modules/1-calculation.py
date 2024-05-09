fb08f0a92e1f7940c99782c7d5875f3826fb1a79
nachoneca <8666@holbertonstudents.com>
2024-05-09 16:38:00 +0000

    Fix of 1-calculation.py

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -4,11 +4,7 @@ if __name__ == "__main__":
     a = 10
     b = 5
     from calculator_1 import add, sub, mul, div
-    resa = add(a, b)
-    ress = sub(a, b)
-    resm = mul(a, b)
-    resd = div(a, b)
-    print("{} + {}= {}".format(a, b, resa))
-    print("{} - {}= {}".formar(a, b, ress))
-    print("{} x {}= {}".format(a, b, resm))
-    print("{} / {}= {}".format(a, b, resd))
+    print("{} + {}= {}".format(a, b, add(a, b)))
+    print("{} - {}= {}".formar(a, b, sub(a, b)))
+    print("{} x {}= {}".format(a, b, mul(a, b)))
+    print("{} / {}= {}".format(a, b, div(a, b)))
