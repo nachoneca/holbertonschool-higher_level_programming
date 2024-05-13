21a1ff4bcaa9cdd3ae23ffadf560f621aa0dc2fb
nachoneca <8666@holbertonstudents.com>
2024-05-13 21:12:17 +0000

    Creation of 2-replace_in_list.py

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -5,7 +5,5 @@ def replace_in_list(my_list, idx, element):
     elif idx > len(my_list) - 1:
         return my_list
     else:
-        for i in my_list:
-            if i == idx:
-                my_list[i] = element
+        my_list[idx] = element
     return my_list
