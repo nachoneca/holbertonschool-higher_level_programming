45adda53a95f4b925b51927caf4015fb4a47b134
nachoneca <8666@holbertonstudents.com>
2024-05-13 21:19:27 +0000

    Creation of 3-print_reversed_list_integer.py

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    rev_list = my_list[::-1]
-    for i in rev_list
+    for i in reversed(my_list)
         print(i)
-        print()
