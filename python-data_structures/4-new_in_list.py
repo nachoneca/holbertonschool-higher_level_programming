f572abe1fdba949ae804b58c0f43c564c171a652
nachoneca <8666@holbertonstudents.com>
2024-05-13 21:31:08 +0000

    Creation of 4-new_in_list.py

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -3,7 +3,7 @@ def new_in_list(my_list, idx, element):
     new_list = my_list.copy()
     if idx < 0:
         return my_list
-    if idx > len(my_list):
+    if idx > len(my_list) - 1:
         return my_list
     else:
         new_list[idx] = element
