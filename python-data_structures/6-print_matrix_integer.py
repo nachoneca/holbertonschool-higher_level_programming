b103a91d762e333031532e4ed09ba85a28ad0ee8
nachoneca <8666@holbertonstudents.com>
2024-05-13 22:01:59 +0000

    Creation of 6-print_matrix_integer.py

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        for element in row:
-            print("{:d}".format(element), end=" ")
-        print()
+    for i in matrix:
+        print(" ".join("{:d}".format(j) for j in i))
