54e45c34ffbf272009e5363da7052927c9b38f3a
nachoneca <8666@holbertonstudents.com>
2024-05-16 17:29:05 +0000

    Creation of 3-safe-print_division.py

---
+++ python-exceptions/3-safe_print_division.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def safe_print_division(a, b):
+    try:
+        div = a / b
+    except (TypeError, ValueError, ZeroDivisionError, Exception):
+        return(None)
+    else:
+        return div
+    finally:
+        print("Inside result: {}".format(div))
