0def939653e14573fd0a9e3a35d37fd3391fdefe
nachoneca <8666@holbertonstudents.com>
2024-05-16 17:45:16 +0000

    Fix

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -7,10 +7,8 @@ def safe_print_list_integers(my_list=[], x=0):
             if int(my_list[i]):
                 print("{:d}".format(my_list[i]), end="")
                 num += 1
-        except (ValueError, TypeError):
+        except (ValueError, TypeError, IndexError):
             continue
-        except IndexError:
-            break
     print()
     return num
 
