48fef7a5ec2bf661001f674290f75ea98f287343
nachoneca <8666@holbertonstudents.com>
2024-05-16 16:37:28 +0000

    Creation of 1-safe_print_integer.py

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,7 @@
 #!7usr/bin/python3
 def safe_print_integer(value):
     try:
-        value_int = int(value)
-        print("{:d}".format(value_int))
+        print("{:d}".format(value))
         return True
     except ValueError:
         return False
