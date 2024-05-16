172d70ea02b9cc4b9bf46bed8932d03d7937cfe2
nachoneca <8666@holbertonstudents.com>
2024-05-16 17:06:41 +0000

    Creation of 2-safe_print_list_integers.py

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    num = 0
+    for i in range(x):
+        try:
+            if int(my_list[i]):
+                print("{:d}".format(my_list[i]), end="")
+                num += 1
+        except (ValueError, TypeError):
+            continue
+    print()
+    return num
+
