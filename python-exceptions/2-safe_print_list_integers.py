cab60190692bc0ef8e8333508e81207551b0fc50
nachoneca <8666@holbertonstudents.com>
2024-05-16 17:44:10 +0000

    Fix

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -9,6 +9,8 @@ def safe_print_list_integers(my_list=[], x=0):
                 num += 1
         except (ValueError, TypeError):
             continue
+        except IndexError:
+            break
     print()
     return num
 
