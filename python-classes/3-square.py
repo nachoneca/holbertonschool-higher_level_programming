dc2848a81c0897d0649db0e390b4caf56802da65
nachoneca <8666@holbertonstudents.com>
2024-05-19 21:10:56 +0000

    Fix 3-square.py

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,14 +1,29 @@
 #!/usr/bin/python3
 """This module defines an empty class named 'Square'"""
+
+
 class Square:
-    """This is the definition of the 'Square' class"""
+    """Represents a square."""
     def __init__(self, size=0):
-        """Instance of Size"""
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of the square (default is 0).
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
     def area(self):
-        """Definition of Area"""
+        """Calculate the area of the square.
+
+        Returns:
+            int: The area of the square.
+        """
         return int(self.__size) * int(self.__size)
