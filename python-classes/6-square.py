19439f3a818601cb752d5f0e45a93ca0f7903aa1
nachoneca <8666@holbertonstudents.com>
2024-05-20 00:22:55 +0000

    Fix 6-square.py

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -80,7 +80,7 @@ class Square:
         if self.__size == 0:
             print("")
             return
-        for i in range(self.__position[1])
+        for i in range(self.__position[1]):
             print("")
         for i in range(0, self.__size):
             print(" " * self.__position[0] + "#" * self.__size)
