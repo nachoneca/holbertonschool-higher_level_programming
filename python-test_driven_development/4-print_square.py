b7eb128de57f165227ec1061dda991ec0e07f92f
nachoneca <8666@holbertonstudents.com>
2024-05-21 15:31:55 +0000

    Creation of 4-print_square.py

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -4,12 +4,10 @@
 
 def print_square(size):
     """ print a square """
-    int_error = "size must be an integer"
-    neg_error = "size must be >= 0"
     if type(size) is not int:
-        raise TypeError(int_error)
+        raise TypeError("size must be an integer")
     if size < 0:
-        raise ValueError(neg_error)
+        raise ValueError("size must be >= 0")
     for i in range(size):
         print("#" * size, end="")
         print()
