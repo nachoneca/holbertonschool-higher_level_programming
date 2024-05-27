0a024ce04369bba07fefae4c5b9b14aaae1a3bf3
nachoneca <8666@holbertonstudents.com>
2024-05-27 21:51:24 +0000

    Incorporation of 7...

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,22 +1,34 @@
 #!/usr/bin/python3
-"""adds all arguments to a Python list, and then save them to a file"""
+""" 
+This script adds all arguments passed to it from the command line to a Python list, 
+and then saves them to a file in JSON format. 
+"""
 
+# Importamos los módulos necesarios
+import sys  # Importamos el módulo sys para manejar los argumentos de la línea de comandos
+import os   # Importamos el módulo os para manejar operaciones del sistema operativo
 
+# Importamos las funciones necesarias desde otros módulos
+from save_to_json_file import save_to_json_file  # Importamos la función para guardar datos en un archivo JSON
+from load_from_json_file import load_from_json_file  # Importamos la función para cargar datos desde un archivo JSON
 
-import sys
-import os
-from save_to_json_file import save_to_json_file
-from load_from_json_file import load_from_json_file
-
-
+# Definimos el nombre del archivo donde se guardarán los datos
 filename = "add_item.json"
+
+# Obtenemos todos los argumentos pasados al script desde la línea de comandos, excluyendo el nombre del script
 args = sys.argv[1:]
 
+# Verificamos si el archivo JSON ya existe
 if os.path.exists(filename):
+    # Si el archivo existe, cargamos su contenido (debería ser una lista) utilizando la función load_from_json_file
     items = load_from_json_file(filename)
 else:
+    # Si el archivo no existe, inicializamos una lista vacía para almacenar los elementos
     items = []
 
+# Añadimos los argumentos de la línea de comandos a la lista cargada o a la lista vacía
 items.extend(args)
 
+# Guardamos la lista actualizada en el archivo JSON utilizando la función save_to_json_file
 save_to_json_file(items, filename)
+
