07ab5e0140e043ac2780b6a2d0c34b1bacd40204
nachoneca <8666@holbertonstudents.com>
2024-05-27 18:40:14 +0000

    Incorporation of 6-load_from_json_file.py

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -5,5 +5,5 @@ import json
 
 def load_from_json_file(filename):
     """ creates an Object from a JSON file"""
-    with open(filename, mode="w", encoding="utf-8") as my_file:
+    with open(filename, mode="r", encoding="utf-8") as my_file:
         return json.load(my_file)
