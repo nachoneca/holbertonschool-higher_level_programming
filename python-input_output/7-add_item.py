6e0fd4da2362d74dc80cf93dbe7f44f896da1c98
nachoneca <8666@holbertonstudents.com>
2024-05-27 21:52:49 +0000

    Incorporation of 7...

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -7,17 +7,17 @@ and then saves them to a file in JSON format.
 # Importamos los módulos necesarios
 import sys  # Importamos el módulo sys para manejar los argumentos de la línea de comandos
 import os   # Importamos el módulo os para manejar operaciones del sistema operativo
-
+""" json"""
 # Importamos las funciones necesarias desde otros módulos
 from save_to_json_file import save_to_json_file  # Importamos la función para guardar datos en un archivo JSON
 from load_from_json_file import load_from_json_file  # Importamos la función para cargar datos desde un archivo JSON
-
+"""json"""
 # Definimos el nombre del archivo donde se guardarán los datos
 filename = "add_item.json"
-
+"""json"""
 # Obtenemos todos los argumentos pasados al script desde la línea de comandos, excluyendo el nombre del script
 args = sys.argv[1:]
-
+"""json"""
 # Verificamos si el archivo JSON ya existe
 if os.path.exists(filename):
     # Si el archivo existe, cargamos su contenido (debería ser una lista) utilizando la función load_from_json_file
