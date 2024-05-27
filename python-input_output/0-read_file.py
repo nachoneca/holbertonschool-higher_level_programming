be8e3796920dd17789e694f88d4952a99110a404
nachoneca <8666@holbertonstudents.com>
2024-05-27 17:52:25 +0000

    creation of 0-read_file.py

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
+"""Creation of filereder"""
+
 
 def read_file(filename=""):
+    """File reader"""
     with open("filename", encoding="utf-8") as my_file:
         print(my_file.read())
