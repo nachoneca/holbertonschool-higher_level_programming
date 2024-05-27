0e3b6c303dddd0cd8c91c6f691694b473dbc83f0
nachoneca <8666@holbertonstudents.com>
2024-05-27 21:44:38 +0000

    Incorporation of 7...

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""adds all arguments to a Python list, and then save them to a file"""
+import sys
+import os
+from save_to_json_file import save_to_json_file
+from load_from_json_file import load_from_json_file
+
+
+filename = "add_item.json"
+args = sys.argv[1:]
+
+if os.path.exists(filename):
+    items = load_from_json_file(filename)
+else:
+    items = []
+
+items.extend(args)
+
+save_to_json_file(items, filename)
