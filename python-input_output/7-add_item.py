ea831e3108e34f6cc900ad4fa7b01bff5fc9d5d8
nachoneca <8666@holbertonstudents.com>
2024-05-31 19:13:29 +0000

    Fix 7...

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,34 +1,25 @@
 #!/usr/bin/python3
-""" 
-This script adds all arguments passed to it from the command line to a Python list, 
-and then saves them to a file in JSON format. 
+
+"""
+Script add all arguments in a file
 """
 
-# Importamos los módulos necesarios
-import sys  # Importamos el módulo sys para manejar los argumentos de la línea de comandos
-import os   # Importamos el módulo os para manejar operaciones del sistema operativo
-""" json"""
-# Importamos las funciones necesarias desde otros módulos
-from save_to_json_file import save_to_json_file  # Importamos la función para guardar datos en un archivo JSON
-from load_from_json_file import load_from_json_file  # Importamos la función para cargar datos desde un archivo JSON
-"""json"""
-# Definimos el nombre del archivo donde se guardarán los datos
-filename = "add_item.json"
-"""json"""
-# Obtenemos todos los argumentos pasados al script desde la línea de comandos, excluyendo el nombre del script
-args = sys.argv[1:]
-"""json"""
-# Verificamos si el archivo JSON ya existe
-if os.path.exists(filename):
-    # Si el archivo existe, cargamos su contenido (debería ser una lista) utilizando la función load_from_json_file
-    items = load_from_json_file(filename)
-else:
-    # Si el archivo no existe, inicializamos una lista vacía para almacenar los elementos
-    items = []
 
-# Añadimos los argumentos de la línea de comandos a la lista cargada o a la lista vacía
-items.extend(args)
+import json
+import sys
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-# Guardamos la lista actualizada en el archivo JSON utilizando la función save_to_json_file
-save_to_json_file(items, filename)
+arg = sys.argv[1:]
 
+add_arg = []
+
+try:
+    add_arg = load_from_json_file('add_item.json')
+    for i in arg:
+        add_arg.append(i)
+    save_to_json_file(add_arg, 'add_item.json')
+except FileNotFoundError:
+    for i in arg:
+        add_arg.append(i)
+    save_to_json_file(add_arg, 'add_item.json')
