40a184e7c87484a53ea40aa62f272cd79c55aa9e
nachoneca <8666@holbertonstudents.com>
2024-06-21 13:37:05 -0300

    fix

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,5 +1,2 @@
 --This file creates a table called first_table
-CREATE TABLE IF NOT EXISTS first_table (
-    id INT,
-    name VARCHAR(256)
-);
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
