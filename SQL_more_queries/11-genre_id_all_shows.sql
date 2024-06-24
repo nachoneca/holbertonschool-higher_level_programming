f9ddf9c4bfcedff2ecae79879d7ea1c4b6d838ca
nachoneca <8666@holbertonstudents.com>
2024-06-23 22:06:37 -0300

    creation of file

---
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,4 @@
+-- Joins both tables and if any show dont have a genere id shows as NULL thanks to left join
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
